ff6069ad7a810ca0fb8f3e6c3897229f8dfed7b2
Will Qian <qianwent@hotmail.com>
2017-02-23 18:32:09 -0500

    first commit

---
+++ src/com/leetcode/array/TwoSum.java
@@ -0,0 +1,34 @@
+package com.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] numbers = {2,7,11,15};
+		int target = 9;
+		int result[] = TwoSum.twoSum(numbers, target);
+		for(int r : result) {
+			System.out.println(r);
+		}
+		
+	}
+
+	public static int[] twoSum(int[] numbers, int target) {
+	    int[] result = new int[2];
+	    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	    for (int i = 0; i < numbers.length; i++) {
+	        if (map.containsKey(target - numbers[i])) {
+	            result[1] = i + 1;
+	            result[0] = map.get(target - numbers[i]);
+	            return result;
+	        }
+	        map.put(numbers[i], i + 1);
+	    }
+	    return result;
+	}
+	
+}
