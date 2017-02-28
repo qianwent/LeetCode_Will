a97c39fdd35dc28b5ea2248a7ed5787c645654b5
Will Qian <qianwent@hotmail.com>
2017-02-27 23:40:12 -0500

    leetcode

--- src/com/leetcode/array/TwoSum.java
+++ src/com/leetcode/array/TwoSum.java
@@ -21,12 +21,13 @@ public class TwoSum {
 	    int[] result = new int[2];
 	    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 	    for (int i = 0; i < numbers.length; i++) {
+	    	map.put(numbers[i], i);
 	        if (map.containsKey(target - numbers[i])) {
-	            result[1] = i + 1;
+	            result[1] = i;
 	            result[0] = map.get(target - numbers[i]);
 	            return result;
 	        }
-	        map.put(numbers[i], i + 1);
+//	        map.put(numbers[i], i);
 	    }
 	    return result;
 	}
