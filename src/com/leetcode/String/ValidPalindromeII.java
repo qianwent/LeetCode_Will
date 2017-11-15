43b49478056107496d69b87b5b81e16cf54e611c
Will Qian <qianwent@hotmail.com>
2017-11-15 10:43:34 -0500

    more comments for greedy algorithm

--- src/com/leetcode/String/ValidPalindromeII.java
+++ src/com/leetcode/String/ValidPalindromeII.java
@@ -15,6 +15,14 @@ package com.leetcode.String;
  */
 public class ValidPalindromeII {
 
+    /*
+    本题解法为greedy algorithm
+    贪心算法（又称贪婪算法）是指，在对问题求解时，总是做出在当前看来是最好的选择。
+    也就是说，不从整体最优上加以考虑，他所做出的是在某种意义上的局部最优解。
+贪心算法不是对所有问题都能得到整体最优解，关键是贪心策略的选择，选择的贪心策略必须具备无后效性，
+即某个状态以前的过程不会影响以后的状态，只与当前状态有关。
+    注意：这种算法是特殊条件下才能使用，并不一定能给出最优解，这个后续碰到再看 TODO
+     */
     public boolean validPalindrome(String s) {
         int i = 0, j = s.length()-1;
         //有这种左右指针往中间夹逼的，while循环再合适不过了
