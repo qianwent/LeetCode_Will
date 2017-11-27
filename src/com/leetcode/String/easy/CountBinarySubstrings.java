6656b98fed3618175c5fd5dd971eadc91d550d41
Will Qian <qianwent@hotmail.com>
2017-11-27 18:46:30 -0500

    special case for grouping characters

--- src/com/leetcode/String/easy/CountBinarySubstrings.java
+++ src/com/leetcode/String/easy/CountBinarySubstrings.java
@@ -35,8 +35,38 @@ public class CountBinarySubstrings {
     子集显然就是[2,3], [3,4], [4,6]
     每个子集中满足条件的substring个数就是小的那个数
      */
-//    public int countBinarySubstrings(String s) {
-//
-//    }
+    public static int countBinarySubstrings(String s) {
+        int[] arr = new int[s.length()];
+        int count = 0;
+        arr[count] = 1;
+        int prev = s.charAt(0);
+        for(int i=1; i<s.length(); i++) {
+            if(s.charAt(i) == prev) {
+                arr[count]++;
+            } else {
+                prev = s.charAt(i);
+                count++;
+                arr[count]++;
+//                arr[count++]++;//这么写count还没自增就arr[count]++了，显然不对
+            }
+        }
+        int subStringCount = 0;
+        /*
+        TODO:这里注意一下s.length()，不是s.length()-1
+        为什么？这是长久以来会搞混的第一感觉，必须纠正，因为每次去验证，就有点浪费时间了，这应该是可以形成肌肉记忆的
+        在for loop中，这个边界条件是要小于string的总长度的，-1显然不合理
+        那么什么时候会用到-1？目前为止，用得较多的是在左右指针夹逼的时候，右指针的index是s.length()-1
+        注意：这两个场景区别还是非常明显的！
+         */
+        for(int i=1; i<s.length(); i++) {
+            subStringCount += Math.min(arr[i-1], arr[i]);
+        }
+        return subStringCount;
+    }
+
+    public static void main(String[] args) {
+        int count = countBinarySubstrings("00110");
+        System.out.println(count);
+    }
 
 }
