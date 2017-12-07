520169ece06603c47bbb3b7f35916d8519ce5eff
Will Qian <qianwent@hotmail.com>
2017-12-06 20:42:54 -0500

    insertion sort, interesting

---
+++ src/com/algorithm/topdatastructurealgorithmquestions/sorting/InsertionSortingInterviewTest.java
@@ -0,0 +1,19 @@
+package com.algorithm.topdatastructurealgorithmquestions.sorting;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Wentao Qian on 12/6/2017.
+ */
+public class InsertionSortingInterviewTest {
+    private static List<Integer> sSorted = new LinkedList<>();
+
+    public static void addElement(int e) {
+        // TODO: Insert e to sSorted and make sure sSorted is always sorted.
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
