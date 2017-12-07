e8c7620ebde29c03f8a7dc85c7c94966d3edb0a2
Will Qian <qianwent@hotmail.com>
2017-12-07 17:38:25 -0500

    customize behavior of LinkedList

--- src/com/algorithm/topdatastructurealgorithmquestions/sorting/InsertionSortingInterviewTest.java
+++ src/com/algorithm/topdatastructurealgorithmquestions/sorting/InsertionSortingInterviewTest.java
@@ -1,19 +1,38 @@
 package com.algorithm.topdatastructurealgorithmquestions.sorting;
 
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by Wentao Qian on 12/6/2017.
  */
-public class InsertionSortingInterviewTest {
-    private static List<Integer> sSorted = new LinkedList<>();
+public class InsertionSortingInterviewTest<E extends Comparable> {
+    //TODO: 这里generic用得对不对还有待考量
+    private LinkedList<E> sSorted = new LinkedList<>();
 
-    public static void addElement(int e) {
+    public void addElement(E e) {
         // TODO: Insert e to sSorted and make sure sSorted is always sorted.
+        int i = 0;
+        for(E integer : sSorted) {
+            if(e.compareTo(integer)>0) {//TODO:至少这里我们不能直接比较泛型大小，需要用到compareTo
+                break;
+            }
+            i++;
+        }
+        sSorted.add(i, e);
     }
 
     public static void main(String[] args) {
+        InsertionSortingInterviewTest<Integer> linkedListCustom = new InsertionSortingInterviewTest();
+        linkedListCustom.addElement(3);
+        System.out.println(linkedListCustom.sSorted);
+        linkedListCustom.addElement(1);
+        System.out.println(linkedListCustom.sSorted);
+        linkedListCustom.addElement(5);
+        System.out.println(linkedListCustom.sSorted);
+        linkedListCustom.addElement(2);
+        System.out.println(linkedListCustom.sSorted);
+        linkedListCustom.addElement(0);
+        System.out.println(linkedListCustom.sSorted);
 
     }
 }
