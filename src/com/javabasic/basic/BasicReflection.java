95013996e7ea2337a8bb67b8e85dcdbd29720578
Will Qian <qianwent@hotmail.com>
2017-12-08 11:03:39 -0500

    basic reflection

---
+++ src/com/javabasic/basic/BasicReflection.java
@@ -0,0 +1,19 @@
+package com.javabasic.basic;
+
+/**
+ * Created by Wentao Qian on 12/8/2017.
+ */
+public class BasicReflection {
+    public static void main(String[] args) {
+        try {
+            Class<?> c = Class.forName("some_class");
+            Object oo = c.newInstance();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+}
