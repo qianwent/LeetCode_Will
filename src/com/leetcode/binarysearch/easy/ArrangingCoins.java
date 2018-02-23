6470493a244b3d279ae1d8490f6530130134cb7f
Will Qian <qianwent@hotmail.com>
2018-02-22 21:32:26 -0500

    arrange coins using binary search

--- src/com/leetcode/binarysearch/easy/ArrangingCoins.java
+++ src/com/leetcode/binarysearch/easy/ArrangingCoins.java
@@ -32,6 +32,22 @@ package com.leetcode.binarysearch.easy;
  Because the 4th row is incomplete, we return 3.
  */
 public class ArrangingCoins {
+
+    //TODO:这个思路实际上是用上了数学运算，还需要深入理解一下
+    public int arrangeCoins_bs(int n) {
+        int l=0, r=n;
+        while(l<=r) {
+            int mid = (l+r)/2;
+            if(0.5*mid*mid+0.5*mid<=n) {
+                l = mid +1;
+            } else {
+                r = mid -1;
+            }
+        }
+        return l-1;
+    }
+
+    //这个方法非常直观，似乎使这个问题变成了一个常识，但是我们还是得看下二分查找在这里是怎么用的
     public int arrangeCoins(int n) {
         if(n<1) return 0;
         for(int i=1; ; i++) {
