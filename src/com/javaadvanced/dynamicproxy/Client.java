3f90b06add1040086fcf353970b140356c9fc06f
Will Qian <qianwent@hotmail.com>
2018-02-27 09:21:30 -0500

    use ProxyGeneratorUtils to generate  class bytecode

--- src/com/javaadvanced/dynamicproxy/Client.java
+++ src/com/javaadvanced/dynamicproxy/Client.java
@@ -22,6 +22,9 @@ public class Client {
         subject.rent();
 
         subject.hello("aaa");
+
+        ProxyGeneratorUtils.writeProxyClassToHardDisk("D:/$Proxy0.class");
+
     }
 
 }
---
+++ src/com/javaadvanced/dynamicproxy/ProxyGeneratorUtils.java
@@ -0,0 +1,33 @@
+package com.javaadvanced.dynamicproxy;
+
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Wentao Qian on 2/27/2018.
+ */
+public class ProxyGeneratorUtils {
+
+    public static void writeProxyClassToHardDisk(String path) {
+        byte[] classFile = ProxyGenerator.generateProxyClass("$Proxy0", RealSubject.class.getInterfaces());
+
+        FileOutputStream out = null;
+
+        try {
+            out = new FileOutputStream(path);
+            out.write(classFile);
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
