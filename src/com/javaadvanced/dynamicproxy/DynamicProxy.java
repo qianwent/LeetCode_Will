ce4ab5830e45744c59dabe8753171e901afc9077
Will Qian <qianwent@hotmail.com>
2018-03-01 16:20:21 -0500

    more comments on native method and reflection

--- src/com/javaadvanced/dynamicproxy/DynamicProxy.java
+++ src/com/javaadvanced/dynamicproxy/DynamicProxy.java
@@ -31,7 +31,9 @@ public class DynamicProxy implements InvocationHandler {
 
         System.out.println("Method:" + method);
 
-        // if the proxy object call the method defined on the real object, this method will be called
+        // 从这个invoke方法开始深入探索，就会到NativeMethodAccessorImpl类的native方法，
+        // 最终这就是在调用C写的方法，而这恰恰是反射的最根本的技术，光用debug就算debug到底也没用
+        // private static native Object invoke0(Method var0, Object var1, Object[] var2);
         method.invoke(subject, args);
 
         System.out.println("after invoke");
