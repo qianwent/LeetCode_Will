7db05b1a356af8d76653e222cb5cad0d4f28186e
Will Qian <qianwent@hotmail.com>
2020-07-07 13:32:18 +0800

    Dynamic programming - Palindromic Substrings， first try with two-pointer solution.

---
+++ src/main/java/com/leetcode/dynamicprogramming/medium/PalindromicSubstrings.java
@@ -0,0 +1,58 @@
+package com.leetcode.dynamicprogramming.medium;
+
+/**
+ * Given a string, your task is to count how many palindromic substrings in this string.
+ *
+ * The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+ *
+ * Example 1:
+ *
+ * Input: "abc"
+ * Output: 3
+ * Explanation: Three palindromic strings: "a", "b", "c".
+ *
+ *
+ * Example 2:
+ *
+ * Input: "aaa"
+ * Output: 6
+ * Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ *
+ *
+ * Note:
+ *
+ * The input string length won't exceed 1000.
+ */
+public class PalindromicSubstrings {
+
+    int count = 0;
+
+    /**
+     * 最容易想到的当然是two-pointer
+     * 加上题中明确了，substring如果是不同的index算作不相同，减少了思考的难度
+     * 一个遍历，然后每个i都往两边extend
+     */
+    public int countSubstrings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i+1);
+        }
+        return count;
+    }
+
+    private void extendPalindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
+        }
+    }
+
+    public static void main(String[] args) {
+        PalindromicSubstrings ps = new PalindromicSubstrings();
+        System.out.println(ps.countSubstrings("aaa"));
+    }
+}
